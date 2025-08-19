8f55291a4b3ee4f0d992002f940c8685870d5f4e
athulr477 <athulrj477@gmail.com>
2025-08-19 16:23:37 +0530

    b_1

--- stud.py
+++ stud.py
@@ -2,7 +2,7 @@ students = ["athul","ankith","adith","akshath"]
 print(students)
 
 students_tuple = ("athul","ankith","adith","akshath")
-print("students in class": student_tuple)
+print( "students name :", students_tuple)
 
 
 
